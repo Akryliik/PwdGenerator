5bab9b0e3f8191c875662e5918ea575cb28b56ab
Zboubizte <ken.bres@hotmail.fr>
2019-10-23 09:50:28 +0200

    Modification du parser

--- parser.py
+++ parser.py
@@ -1,50 +1,96 @@
-def main():
+#!/usr/bin/env python
 
-	f= open("data.txt", "r")
-	if f.mode == 'r':
-		contents = f.readlines()
-
-		totalLength = 0
-		clearWord = ""
-		biggest = 0
-		biggestWord=""
-		smallest = 9999999 
-		smallestWord = ""
-		digitWord = 0 
-		strWord = 0
-		tabWordLength = 13 * [0]
+import sys
 
 
-		for word in contents:
-			clearWord = word.rstrip()
-			totalLength += len(clearWord)
+def main(argv):
+	longueurTotale = 0
+	nombreMots = 0
+	motLePlusLong = 0
+	motLePlusCourt = 9999
+	longueurMots = 13 * [0]
+	motsLettres = 0
+	motsChiffre = 0
+	lettresEtChiffres = 0
+	lettresEtSpecial = 0
+	chiffresEtSpecial = 0
+	lettresEtChiffresEtSpecial = 0
 
-			if clearWord.isdigit():
-				digitWord += 1
-			if clearWord.isalpha():
-				strWord += 1
-			if len(clearWord) <= 12:
-				tabWordLength[len(clearWord)] += 1
+	with open(argv[0], 'r') as fichier:
+		lignes = fichier.read().split('\n')
 
-			if len(clearWord) > biggest:
-				biggest = len(clearWord)
-				biggestWord = clearWord
-			if len(clearWord) < smallest:
-				smallest = len(clearWord)
-				smallestWord = clearWord
+		for ligne in lignes:
+			if len(ligne) > 0:
+				nombreMots += 1
+				longueurTotale += len(ligne)
 
-			#print(word)
-		average = float(totalLength) / float(len(contents))
+				if len(ligne) > motLePlusLong:
+					motLePlusLong = len(ligne)
+
+				if len(ligne) < motLePlusCourt:
+					motLePlusCourt = len(ligne)
+
+				if len(ligne) > 12:
+					longueurMots[12] += 1
+				else:
+					longueurMots[len(ligne)] += 1
+
+				if ligne.isalpha():
+					motsLettres += 1
+
+				if ligne.isdigit():
+					motsChiffre += 1
+
+				if contientLettre(ligne) and contientChiffre(ligne) and not contientSpecial(ligne):
+					lettresEtChiffres += 1
+				if contientLettre(ligne) and contientSpecial(ligne) and not contientChiffre(ligne):
+					lettresEtSpecial += 1
+				if contientSpecial(ligne) and contientChiffre(ligne) and not contientLettre(ligne):
+					chiffresEtSpecial += 1
+				if contientSpecial(ligne) and contientChiffre(ligne) and contientLettre(ligne):
+					lettresEtChiffresEtSpecial += 1
+
+	print("Nombre de mots      : " + str(nombreMots))
+	print("Taille moyenne      : " + str(float(longueurTotale) / nombreMots))
+	print("Mot le plus long    : " + str(motLePlusLong))
+	print("Mot le plus court   : " + str(motLePlusCourt))
+	print("Lettres uniquement  : " + str(motsLettres))
+	print("Chiffres uniquement : " + str(motsChiffre))
+	print("Lettres + chiffres  : " + str(lettresEtChiffres))
+	print("Lettres + special   : " + str(lettresEtSpecial))
+	print("Chiffres + special  : " + str(chiffresEtSpecial))
+	print("Lett + chiff + spec : " + str(motsChiffre))
+
+	for i in range(len(longueurMots)):
+		if i != 0:
+			if i < 10:
+				print("Mots de taille " + str(i) + "    : " + str(longueurMots[i]))
+			elif i == 12:
+				print("Mots de taille " + str(i) + "+  : " + str(longueurMots[i]))
+			elif i >= 10:
+				print("Mots de taille " + str(i) + "   : " + str(longueurMots[i]))
 
 
-		print("Biggest word length" + str(biggest)+ ", word "+ str(biggestWord))
-		print("Smallest word length" + str(smallest)+ ", word "+ str(smallestWord))
-		print("Taille moyenne des mots : "+ str(average))
-		print("Mots contenant que des chiffres : "+ str(digitWord))
-		print("Mots contenant que du string : "+ str(strWord))
+def contientLettre(mot):
+	for i in mot:
+		if i.isalpha():
+			return True
+	return False
 
-		for size in range(len(tabWordLength)):
-			print("Mot de taille " + str(size) + " : " + str(tabWordLength[size]))
+
+def contientChiffre(mot):
+	for i in mot:
+		if i.isdigit():
+			return True
+	return False
+
+
+def contientSpecial(mot):
+	for i in mot:
+		if not i.isdigit() and not i.isalpha():
+			return True
+	return False
+
 
 if __name__ == "__main__":
-	main()
+    main(sys.argv[1:])

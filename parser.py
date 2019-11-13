3653ffc289719c95dd4d6ed49f04ab1527cceac2
Akryliik <noaman.t@hotmail.fr>
2019-11-13 11:16:54 +0100

    Ajout des graph et des folders pour les données générés

--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-
+import io
+import webbrowser, os
+import base64
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main(argv):
 	longueurTotale = 0
@@ -75,6 +79,18 @@ def main(argv):
 			elif i >= 10:
 				print("Mots de taille " + str(i) + "   : " + str(longueurMots[i]))
 
+	printHTML( longueurTotale, 
+	nombreMots, 
+	motLePlusLong, 
+	motLePlusCourt,
+	longueurMots,
+	motsLettres, 
+	motsChiffre, 
+	motsSpecial, 
+	lettresEtChiffres, 
+	lettresEtSpecial, 
+	chiffresEtSpecial, 
+	lettresEtChiffresEtSpecial)
 
 def contientLettre(mot):
 	for i in mot:
@@ -96,6 +112,14 @@ def contientSpecial(mot):
 			return True
 	return False
 
+def fig_to_base64(fig):
+    img = io.BytesIO()
+    fig.savefig(img, format='png',
+                bbox_inches='tight')
+    img.seek(0)
+
+    return base64.b64encode(img.getvalue())
+
 def printHTML(longueurTotale, 
 	nombreMots, 
 	motLePlusLong, 
@@ -109,35 +133,58 @@ def printHTML(longueurTotale,
 	chiffresEtSpecial, 
 	lettresEtChiffresEtSpecial):
 
-	html = "<html><head> </head><body>"
+	html = "<html><head><script src='https://unpkg.com/tlx/browser/tlx.js'></script><script src='https://unpkg.com/tlx-chart/browser/tlx-chart.js'></script></head><body>\n"
  	
-	html = html + "<h3> Résultat du parseur </h3><ul>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Taille moyenne      : " + str(float(longueurTotale) / nombreMots) + "</li>"
-	html = html + "<li> Mot le plus long    : " + str(motLePlusLong) +"</li>"
-	html = html + "<li> Mot le plus court   : " + str(motLePlusCourt) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
-	html = html + "<li> Nombre de mots      : " + str(nombreMots) +"</li>"
+	html = html + "<h3> Resultat du parseur fichier : "+ os.path.basename(sys.argv[-1]) +" </h3><table>\n"
+	html = html + "<tr> <td> Nombre de mots      :  </td><td>"  + str(nombreMots) +"</td> </tr>\n"
+	html = html + "<tr> <td> Taille moyenne      :  </td><td>"  + str(float(longueurTotale) / nombreMots) +"</td> </tr>\n"
+	html = html + "<tr> <td> Mot le plus long    :  </td><td>"  + str(motLePlusLong) +"</td> </tr>\n"
+	html = html + "<tr> <td> Mot le plus court   :  </td><td>"  + str(motLePlusCourt) +"</td> </tr>\n"
+	html = html + "<tr> <td> Lettres uniquement  :  </td><td>"  + str(motsLettres) +"</td> </tr>\n"
+	html = html + "<tr> <td> Chiffres uniquement :  </td><td>"  + str(motsChiffre) +"</td> </tr>\n"
+	html = html + "<tr> <td> Special uniquement  :  </td><td>"  + str(motsSpecial) +"</td> </tr>\n"
+	html = html + "<tr> <td> Lettres + chiffres  :  </td><td>"  + str(lettresEtChiffres) +"</td> </tr>\n"
+	html = html + "<tr> <td> Lettres + special   :  </td><td>"  + str(lettresEtSpecial) +"</td> </tr>\n"
+	html = html + "<tr> <td> Chiffres + special  :  </td><td>"  + str(chiffresEtSpecial) +"</td> </tr>\n"
+	html = html + "<tr> <td> Lett + chiff + spec :  </td><td>"  + str(lettresEtChiffresEtSpecial) +"</td> </tr>\n"
+	html = html + "<tr> <td colspan='2'> Longueur des mots : </td><td>\n"
+	html = html + "</table>\n"
 
 
-	print("Lettres uniquement  : " + str(motsLettres))
-	print("Chiffres uniquement : " + str(motsChiffre))
-	print("Special uniquement  : " + str(motsSpecial))
-	print("Lettres + chiffres  : " + str(lettresEtChiffres))
-	print("Lettres + special   : " + str(lettresEtSpecial))
-	print("Chiffres + special  : " + str(chiffresEtSpecial))
-	print("Lett + chiff + spec : " + str(lettresEtChiffresEtSpecial))
-	html = html+"</body></html>"
-	 
-	hs = open("asciiCharHTMLTable.html", 'w')
+	nombre =[]
+	longueur =[]
+	for i in range(len(longueurMots)):	
+		nombre.append(str(i))
+		longueur.append(str(longueurMots[i]))
+
+	index = np.arange(13)
+	fig, ax = plt.subplots()
+	plt.bar(index, longueur, 0.35, 0.8, label='Longueur')
+	plt.xlabel('Taille')
+	plt.ylabel('Nombre de mots')
+	plt.title('Repartition des mots en fonction de leurs tailles.')
+	fig.savefig('graph/tableLongueur'+ os.path.basename(sys.argv[-1]).split('.')[0] +'.png')
+
+	html = html + "<img src=\"../graph/tableLongueur"+ os.path.basename(sys.argv[-1]).split('.')[0] +".png\" style=\"display:block;margin: auto;width: 60%;border: 3px solid black;padding: 10px;\">\n"
+
+	nombre = [motsLettres, motsChiffre, motsSpecial, lettresEtChiffres, lettresEtSpecial, chiffresEtSpecial, lettresEtChiffresEtSpecial]
+	type_ = ["Lettres uniquement", "Chiffres uniquement", "Special uniquement", "Lettres + chiffres", "Lettres + special", "Chiffres + special", "Lett + chiff + spec"]
+	index = np.arange(7)
+	fig, ax = plt.subplots()
+	plt.bar(index, nombre, 0.35, 0.8, label='Type')
+	plt.xlabel('Type')
+	plt.xticks(index, type_, rotation=15)
+	plt.ylabel('Nombre de mots')
+	plt.title('Repartition des mots en fonction de leurs types.')
+	fig.savefig('graph/tableType'+ os.path.basename(sys.argv[-1]).split('.')[0] +'.png')
+	html = html + "<img src=\"../graph/tableType"+ os.path.basename(sys.argv[-1]).split('.')[0] +".png\" style=\"display:block;margin: auto;width: 60%;border: 3px solid black;padding: 10px;\">\n"
+	html = html + "</body></html>\n"
+	hs = open("resultat/"+os.path.basename(sys.argv[-1]).split('.')[0]+".html", 'w')
 	hs.write(html)
-	 
+	
 	print html
+	webbrowser.open('file://' + os.path.realpath("resultat/"+os.path.basename(sys.argv[-1]).split('.')[0]+".html"))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
